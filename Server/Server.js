5a9a6c247688eb6f87be8e354f863a611df700af
PetasoftStudio <83537037+PetasoftStudio@users.noreply.github.com>
2023-06-19 14:29:39 +0300

    Add files via upload

--- Server/Server.js
+++ Server/Server.js
@@ -1,1 +1,25 @@
+const Express = require("express");
+const Serve = Express();
+const App = require("http").createServer(Serve);
+const { Server } = require("ws");
+const PORT = 5000;
 
+
+App.listen(PORT, () => {
+    console.log("Listening on port: " + PORT)
+})
+
+
+var Websocket = new Server({
+    server: App
+})
+
+
+Websocket.addListener("connection", (Client) => {
+    Client.on("message", (message) => {
+        // Send the packet to all clients
+        Websocket.clients.forEach(client => {
+            client.send(message);
+        });
+    })
+})
